51cefe84a22bcc6ff1757943952fea1f7de6f8d1
Gary Lai <garylai1990@gmail.com>
2023-01-07 21:30:25 -0800

    feat: setup ghost 5 for heroku

---
+++ bin/postinstall.sh
@@ -0,0 +1,7 @@
+ghost install --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+cp -Rf package.json package-lock.json node_modules versions/*/
+echo 'INSTALL TO versions/*/ DONE'
+cp -Rf versions/*/* .
+echo 'INSTALL TO PWD DONE'
+bash -x bin/aws-s3.sh
+bash -x bin/themes.sh

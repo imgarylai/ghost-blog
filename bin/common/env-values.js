1d1fd6431813c43845eac96f59909bc649b3c24d
Gary Lai <garylai1990@gmail.com>
2023-01-12 20:06:05 -0800

    feat(mail service): support general mail service

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -4,4 +4,21 @@ module.exports = {
     process.env.JAWSDB_URL ||
     process.env.CLEARDB_DATABASE_URL ||
     process.env.JAWSDB_MARIA_URL,
+  mailService:
+    process.env.MAIL_SERVICE ||
+    "Mailgun",
+  host:
+    process.env.SMTP_HOST ||
+    process.env.SMTP_SERVER ||
+    process.env.MAILGUN_SMTP_SERVER,
+  port:
+    process.env.SMTP_PORT ||
+    process.env.MAILGUN_SMTP_PORT ||
+    "465",
+  smtpLogin:
+    process.env.SMTP_LOGIN ||
+    process.env.MAILGUN_SMTP_LOGIN,
+  smtpPassword:
+    process.env.SMTP_PASSWORD ||
+    process.env.MAILGUN_SMTP_PASSWORD
 };

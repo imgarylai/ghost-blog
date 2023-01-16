d770900c52f252d69fada1765a826a042ea7a897
Gary Lai <garylai1990@gmail.com>
2023-01-16 15:55:44 -0800

    smtp env

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -4,4 +4,12 @@ module.exports = {
     process.env.JAWSDB_URL ||
     process.env.CLEARDB_DATABASE_URL ||
     process.env.JAWSDB_MARIA_URL,
+  mail: {
+    service: process.env.SMTP_MAIL_SERVICE ||
+      "Mailgun",
+    smtpLogin: process.env.SMTP_LOGIN ||
+      process.env.MAILGUN_SMTP_LOGIN,
+    smtpPass: process.env.SMTP_PASSWORD ||
+      process.env.MAILGUN_SMTP_PASSWORD
+  }
 };

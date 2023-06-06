45dadd45b05d4d3f3b3253dfaab5798ef2c443ba
Gary Lai <garylai@amazon.com>
2023-06-06 10:14:49 -0700

    ci(ghost): bump version to 5.50.1

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,4 @@
-ghost install 5.50.0 --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+ghost install 5.50.1 --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
 cp -Rf package.json package-lock.json node_modules versions/*/
 echo 'INSTALL TO versions/*/ DONE'
 cp -Rf versions/*/* .

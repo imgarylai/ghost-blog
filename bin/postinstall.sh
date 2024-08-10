2fe4c9097116694270625279ef357c7b3e767d40
Gary Lai <garylai1990@gmail.com>
2024-08-10 11:32:32 -0700

    ci(ghost): bump version to 5.89.1

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,7 +1,7 @@
-ghost install 5.79.6 --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
-cp -Rf package.json package-lock.json node_modules versions/5.79.6/
+ghost install 5.89.1 --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+cp -Rf package.json package-lock.json node_modules versions/5.89.1/
 echo 'INSTALL TO versions/*/ DONE'
-cp -Rf versions/5.79.6/* .
+cp -Rf versions/5.89.1/* .
 echo 'INSTALL TO PWD DONE'
 bash -x bin/aws-s3.sh
 bash -x bin/themes.sh

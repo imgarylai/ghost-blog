a63c55ccfa23a1029db3bd2ec1caeefc4b6538b3
Gary Lai <garylai1990@gmail.com>
2024-08-10 11:24:04 -0700

    fix(heroku): Fix heroku build out of space issue

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,7 +1,8 @@
-ghost install 5.79.6 --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
-cp -Rf package.json package-lock.json node_modules versions/*/
+ghost install 5.79.6 --dir /app/build --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+cp -Rf package.json package-lock.json node_modules /app/build/versions/*/
 echo 'INSTALL TO versions/*/ DONE'
-cp -Rf versions/*/* .
+cp -Rf /app/build/versions/*/* .
+rm -rf /app/build
 echo 'INSTALL TO PWD DONE'
 bash -x bin/aws-s3.sh
 bash -x bin/themes.sh

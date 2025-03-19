94e555b87a93763aa6f4f3b8157fc9d5deca0afe
Gary Lai <garylai1990@gmail.com>
2025-03-18 21:14:21 -0700

    feat(ghost): bump version to 5.112.0

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,4 @@
-ghost install 5.109.6 --dir /app/build --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+ghost install 5.112.0 --dir /app/build --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
 cp -Rf package.json package-lock.json node_modules /app/build/versions/*/
 echo 'INSTALL TO versions/*/ DONE'
 cp -Rf /app/build/versions/*/* .

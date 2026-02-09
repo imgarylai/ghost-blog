d302cbef156deedd354076c89e5fd6bd0678b346
imgarylai <1407908+imgarylai@users.noreply.github.com>
2026-02-09 00:45:27 +0000

    chore: update Ghost to version 6.17.0

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,4 @@
-ghost install 6.14.0 --dir /app/build --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
+ghost install 6.17.0 --dir /app/build --allow-root --no-check-empty --no-prompt --no-stack --no-setup --no-setup-linux-user --db sqlite3
 cp -Rf package.json package-lock.json node_modules /app/build/versions/*/
 echo 'INSTALL TO versions/*/ DONE'
 cp -Rf /app/build/versions/*/* .
